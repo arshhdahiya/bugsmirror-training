a05b23b86b406738a935dc5be52b02c5b513660a
arshhdahiya <arshhdahiya@gmail.com>
2024-01-02 18:08:35 +0530

    initializing repo

---
+++ Variables/variables.go
@@ -0,0 +1,29 @@
+package variables
+
+import "sync"
+
+type User struct {
+	ID         string              `json:"id"`
+	SecretCode string              `json:"secret_code"`
+	Name       string              `json:"name"`
+	Email      string              `json:"email"`
+	Playlists  map[string]Playlist `json:"playlists"`
+}
+
+type Playlist struct {
+	ID    string `json:"id"`
+	Name  string `json:"name"`
+	Songs []Song `json:"songs"`
+}
+
+type Song struct {
+	ID       string `json:"id"`
+	Name     string `json:"name"`
+	Composer string `json:"composer"`
+	MusicURL string `json:"music_url"`
+}
+
+var (
+	users   map[string]User
+	usersMu sync.Mutex
+)
